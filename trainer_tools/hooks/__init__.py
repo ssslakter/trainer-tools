03fac6492b5c79cc23bd66bf5b26bcc067b53d9f
Slava Chaunin <67190162+Ssslakter@users.noreply.github.com>
2026-01-20 17:50:30 +0300

    fix imports

--- trainer_tools/hooks/__init__.py
+++ trainer_tools/hooks/__init__.py
@@ -1,14 +1,10 @@
 from .base import BaseHook
 from .metrics import MetricsHook
-from .metrics_diffusion import DiffusionMetricsHook
 from .pbar import ProgressBarHook
 from .optimization import LRSchedulerHook, AMPHook, GradClipHook
 from .checkpoint import CheckpointHook
-from .sampling import *
-from .text import *
 from .ema import EMAHook
-from .toy import ToyMetricsHook, SequenceSamplingHook
-from ...utils import is_notebook
+from ..utils import is_notebook
 
 import matplotlib
 
